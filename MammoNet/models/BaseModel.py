b04477782c0bcf1ba8d1b855218dd076c875fd27
ja-nina <zukowskanina1@gmail.com>
2024-11-12 13:50:16 +0100

    move wandb tracking to training

--- MammoNet/models/BaseModel.py
+++ MammoNet/models/BaseModel.py
@@ -2,23 +2,25 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from tqdm import tqdm
 import wandb
-from MammoNet.global_variables import RESULTS_DIR, SEED
-from MammoNet.utils import create_results_dir, setup_wandb, set_seed
+from tqdm import tqdm
+from MammoNet.utils.global_variables import RESULTS_DIR, SEED
+from MammoNet.utils.utils import create_results_dir, setup_wandb, set_seed
 
 
 class BaseModel:
     def __init__(self, model, num_classes, model_name, lr=0.001, epochs=10):
+        self.model_name = model_name
         self.model = model
+        self.num_classes = num_classes
+        self.lr = lr
+        self.epochs = epochs
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.epochs = epochs
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
         self.results_dir = RESULTS_DIR
 
         create_results_dir(self.results_dir)
-        setup_wandb("mammonet_project", {"model": model_name, "epochs": self.epochs})
         set_seed(SEED)
 
     def train_epoch(self, train_loader):
@@ -52,6 +54,9 @@ class BaseModel:
         return running_loss / len(val_loader), accuracy
 
     def train(self, train_loader, val_loader, test_loader):
+        # init wandb
+        setup_wandb("mammonet_project", {"model": self.model_name, "epochs": self.epochs})
+        
         best_val_loss = float("inf")
         for epoch in range(self.epochs):
             train_loss = self.train_epoch(train_loader)
@@ -75,4 +80,9 @@ class BaseModel:
 
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
-                torch.save(self.model.state_dict(), os.path.join(self.results_dir, f"{self.model.name}.pth"))
+                torch.save(self.model.state_dict(), os.path.join(self.results_dir, f"{self.model.name}.pth"))         
+        wandb.finish()
+                
+    def load_model(self, model_path, device):
+        self.model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+        self.model.eval()
