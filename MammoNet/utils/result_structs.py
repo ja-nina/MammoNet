f11031e5041514d5ef2c437e9035e69ff62f871c
ja-nina <zukowskanina1@gmail.com>
2024-11-12 14:45:31 +0100

    remove example code for struct

--- MammoNet/utils/result_structs.py
+++ MammoNet/utils/result_structs.py
@@ -38,21 +38,3 @@ class ResultsConfig(BaseModel):
     path_model: PathType
     metrics: Metrics
     image_results: List[ImageResult]
-
-# Example usage
-if __name__ == "__main__":
-    image_result = ImageResult(image_path=Path("path/to/image.png"), label=1, prediction=0)
-    metrics = Metrics(
-        accuracy=0.9,
-        precision=0.8,
-        recall=0.7,
-        f1_score=0.75,
-        confusion_matrix=np.array([[50, 10], [5, 35]]),
-        classification_report="classification report"
-    )
-    results_config = ResultsConfig(
-        path_model=Path("path/to/model.pth"),
-        metrics=metrics,
-        image_results=[image_result]
-    )
-    results_config.model_dump()
